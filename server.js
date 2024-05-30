b95ed7d4e574d8ad17e2491237f67905f82c9289
Najib Muhammadi <najibmuhammadi003@gmail.com>
2024-05-30 10:46:21 +0200

    Products går att hämtas från db

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 import errorHandlerMiddleware from './middleware/errorHandler.js';
 import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 dotenv.config();
 
 const app = express();
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 7070;
 app.use(express.json());
 app.use(cors());
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
