a045814bc12755cac8b94a2d8f3161354f66e21d
Jaerker <johanjakberger@gmail.com>
2024-06-04 15:08:53 +0200

    update

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -9,11 +9,17 @@ const aboutInfo = async (req, res) => {
     try {
         
         // const textInfo = await aboutDb.insert({ 
-        //     'desc': 'Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice.' 
+        //     'description': 'Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice.' 
         // });
         
         const textInfo = await aboutDb.findOne({ _id: 'zCGcKgM2UNsUfG6T' })
-        res.status(200).send(textInfo);
+        res.status(200)
+            .json({
+                success: true,
+                message: 'Found text',
+                status: 200,
+                data: textInfo
+            });
     } catch (error) {
         res.status(500).send({ error: 'Failed to insert info' });
     }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,13 +6,22 @@ import AuthController from '../controllers/authController.js';
 const router = Router();
 
 const controller = new AuthController();
-router.get('/users',authenticateMiddleware.checkUser, validateMiddleware.users.isAdmin, controller.getAllUsers)
+router.get('/users',
+    authenticateMiddleware.checkUser, 
+    validateMiddleware.users.isAdmin, 
+    controller.getAllUsers)
 
-router.get('/users/:userId',authenticateMiddleware.checkUser, controller.getUser);
+router.get('/users/:userId',
+    authenticateMiddleware.checkUser, 
+    controller.getUser);
 
-router.post('/register', validateMiddleware.users.register, controller.registerUser);
+router.post('/register', 
+    validateMiddleware.users.register, 
+    controller.registerUser);
 
-router.post('/login',validateMiddleware.users.login, controller.loginUser);
+router.post('/login',
+    validateMiddleware.users.login, 
+    controller.loginUser);
 
 
 
